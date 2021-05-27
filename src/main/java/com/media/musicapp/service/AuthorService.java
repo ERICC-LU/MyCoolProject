6297f594daff7e213f66d084cdb0a4b13044985f
ERICC-LU <cakeholmes@gmail.com>
2021-05-26 17:57:48 -0700

    First commit.

---
+++ src/main/java/com/media/musicapp/service/AuthorService.java
@@ -0,0 +1,8 @@
+package com.media.musicapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+
+}
---
+++ src/test/java/com/media/musicapp/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
