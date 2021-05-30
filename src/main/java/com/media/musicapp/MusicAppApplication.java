c20c4d6bb7bc6311ab786d4389891d8fde82167f
ERICC-LU <cakeholmes@gmail.com>
2021-05-30 16:28:37 -0700

    Quick fix for latency.

--- src/main/java/com/media/musicapp/MusicAppApplication.java
+++ src/main/java/com/media/musicapp/MusicAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicAppApplication {
     public static void main(String[] args) {
+        System.out.println("This is a fix.");
         SpringApplication.run(MusicAppApplication.class, args);
     }
 
