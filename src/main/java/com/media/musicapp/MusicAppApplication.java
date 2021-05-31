bf957391d3358ec698e7ac021ee93b05f148bfcd
ERICC-LU <cakeholmes@gmail.com>
2021-05-30 17:25:41 -0700

    Another fix

--- src/main/java/com/media/musicapp/MusicAppApplication.java
+++ src/main/java/com/media/musicapp/MusicAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusicAppApplication {
     public static void main(String[] args) {
         System.out.println("This is a fix.");
+        System.out.println("Another fix");
         SpringApplication.run(MusicAppApplication.class, args);
     }
 
